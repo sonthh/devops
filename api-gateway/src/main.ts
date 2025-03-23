e2211edca3cd208dfaf4c7e6028e1f8c3d32cd43
sonthh <tranhuuhongson@gmail.com>
2025-03-23 15:56:19 +0700

    add devops-frontend

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -6,6 +6,18 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
+  app.enableCors({
+    origin: [
+      'http://tuanhung.xyz',
+      'https://tuanhung.xyz',
+      'http://34.8.136.29',
+      'https://34.8.136.29',
+    ],
+    methods: '*',
+    allowedHeaders: '*',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
